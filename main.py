8ad994363a09a82eba77fd9bcccfd308eb6698ed
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-03-01 05:33:01 +0000

    routes

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
+home_page = '''
+    <a href="/about">ABOUTE</a> <br> <br>
+    <a href="/product">PRODUCT</a> <br> <br>
+    <a href="/help">HELP</a>
+'''
+
 @app.route('/')
 def home():
-    return "home page"
+    return home_page
+
+    
+@app.route('/about')
+def about():
+    return '<a href="/">HOME</a>'
+
+
+@app.route('/product')
+def product():
+    return '<a href="/">HOME</a>'
+
+@app.route('/help')
+def help():
+    return '<a href="/">HOME</a>'
 
 app.run(debug=True)
