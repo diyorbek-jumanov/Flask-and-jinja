008a9ead32a5e05a64728370500f03a332c2f43a
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-03-01 06:14:06 +0000

    links

--- main.py
+++ main.py
@@ -3,55 +3,39 @@ from jinja2 import Template
 
 app = Flask(__name__)
 
-home_page = Template('''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>{{title}}</title>
-    </head>
-    <body>
-        <h1>{{content}}</h1>
-        {{link}}
-    </body>
-    </html>
-    ''')
-
 @app.route('/')
 def home():
     title = 'home'
     content = 'home page'
+    link = 'Home'
 
-    return render_template('index.html', title=title, content=content)
+    return render_template('index.html', title=title, content=content, link=link)
 
     
 @app.route('/about')
 def about():
     title = 'about'
     content = 'about page'
-    link = '''
-        <a href="/">HOME</a>
-    '''
-    return home_page.render(title=title, content=content, link=link)
+    link = 'home'
+    
+    return render_template('index.html', title=title, content=content, link=link)
 
 
 @app.route('/product')
 def product():
     title = 'product'
     content = 'product page'
-    link = '''
-        <a href="/">HOME</a>
-    '''
-    return home_page.render(title=title, content=content, link=link)
+    link = 'home'
+
+    return render_template('index.html', title=title, content=content, link=link)
 
 
 @app.route('/help')
 def help():
     title = 'help'
     content = 'help page'
-    link = '''
-        <a href="/">HOME</a>
-    '''
-    return home_page.render(title=title, content=content, link=link)
+    link = 'home'
+
+    return render_template('index.html', title=title, content=content, link=link)
 
 app.run(debug=True)
