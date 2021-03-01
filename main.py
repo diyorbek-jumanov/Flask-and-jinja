f54c5eb8d63f1d79703b34d435c26f97fba38902
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-03-01 05:50:36 +0000

    template

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
 from flask import Flask
+from jinja2 import Template
 
 app = Flask(__name__)
 
+home_page = Template('''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>{{title}}</title>
+    </head>
+    <body>
+        <h1>{{content}}</h1>
+        {{link}}
+    </body>
+    </html>
+    ''')
+
 @app.route('/')
 def home():
-    title = 'index'
+    title = 'home'
     content = 'home page'
     link = '''
         <a href="/about">ABOUTE</a> <br> <br>
         <a href="/product">PRODUCT</a> <br> <br>
         <a href="/help">HELP</a>
     '''
-    home_page = f'''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>{title}</title>
-    </head>
-    <body>
-        <h1>{content}</h1>
-        {link}
-    </body>
-    </html>
-    '''
 
-    return home_page
+    return home_page.render(title=title, content=content, link=link)
 
     
 @app.route('/about')
 def about():
-    return '<a href="/">HOME</a>'
+    title = 'about'
+    content = 'about page'
+    link = '''
+        <a href="/">HOME</a>
+    '''
+    return home_page.render(title=title, content=content, link=link)
 
 
 @app.route('/product')
 def product():
-    return '<a href="/">HOME</a>'
+    title = 'product'
+    content = 'product page'
+    link = '''
+        <a href="/">HOME</a>
+    '''
+    return home_page.render(title=title, content=content, link=link)
+
 
 @app.route('/help')
 def help():
-    return '<a href="/">HOME</a>'
+    title = 'help'
+    content = 'help page'
+    link = '''
+        <a href="/">HOME</a>
+    '''
+    return home_page.render(title=title, content=content, link=link)
 
 app.run(debug=True)
